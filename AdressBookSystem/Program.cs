20503c8fa617c7d6463560d099f5cb05393ab857
Shailesh kumar Satapathy <shaileshsatapathy9@gmail.com>
2021-04-24 11:47:53 +0530

    welcome message added

---
+++ AdressBookSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Addressbook System");
+        }
+    }
+}
